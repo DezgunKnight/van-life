aef458b85fb6b1cda943fc79ad9a3cffbb60a455
DezgunKnight <80746652+DezgunKnight@users.noreply.github.com>
2026-08-31 18:25:02 +0200

    I added and built VanDetail page.
    i used: useParams(), useState, useEffect, Link, Fetch().

--- src/Pages/VanDetail.jsx
+++ src/Pages/VanDetail.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
-import { useParams } from 'react-router'
+import { useState, useEffect } from 'react'
+import { useParams, Link } from 'react-router'
+import './VanDetail.css'
 
 
 export function VanDetail() {
   const params = useParams()
-  console.log(params)
+
+  const [van, setVan] = useState(null)
+
+  useEffect(() => {
+    fetch(`/api/vans/${params.id}`)
+    .then(res => res.json())
+    .then(data => setVan(data.vans))
+  }, [params.id])
 
   return (
-    <h1>Hello Vans</h1>
+    <div className="van-detail-container">
+      <Link to="/vans" className="back-button">&larr; <span>Back to all vans</span></Link>
+      
+      {van ? (
+        <div className="van-detail">
+          <img src={van.imageUrl} alt={van.name} />
+          <i className={`van-type ${van.type} selected`} >{van.type}</i>
+          <h2>{van.name}</h2>
+          <p className="van-price"><span>${van.price}/day</span> </p>
+          <p>{van.description}</p>
+          <button className="link-button">Rent this van</button>
+        </div>
+      ) : <h2>Loading...</h2>}
+    </div>
   )
 }
