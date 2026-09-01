f878eace8981cc6b7ac285d7309a7de9b252e98d
DezgunKnight <80746652+DezgunKnight@users.noreply.github.com>
2026-09-01 17:09:22 +0200

    intro to nested route
    removed navbar component and added layout and header components

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import './App.css'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import { Home } from './Pages/Home'
 import { About } from './Pages/About'
-import { Navbar } from './components/Navbar'
 import { Vans } from './Pages/Vans'
 import { VanDetail } from './Pages/VanDetail'
+import { Layout } from './components/Layout'
 
 import './server'
 
@@ -13,14 +13,13 @@ function App() {
   return (
     <>
     <BrowserRouter>
-      <Navbar />
-
-
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />}/>
+        </Route>
       </Routes>
     </BrowserRouter>      
     </>
---
+++ src/components/Layout.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+
+export function Layout() {
+  
+  return (
+    <h1>Hello pussies</h1>
+  )
+}
